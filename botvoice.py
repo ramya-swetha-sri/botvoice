4e6217615b14fff44789e1c2d509c657097a332b
ramya-swetha-sri <ramyaswethasri@gmail.com>
2026-07-13 15:14:28 +0530

    date and time

--- botvoice.py
+++ botvoice.py
@@ -1,6 +1,8 @@
 import pyttsx3
 import speech_recognition as sr
 import mtranslate
+import datetime
+from datetime import date
 
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
@@ -30,12 +32,25 @@ def command():
             print(e)
             print("say that again please...")
             return "content"
+        
+
+def time():
+    print('Hello! Its time!', datetime.now())
+
+
 def main_proccess():
     while True:
         request = command()
         if "wake up" in request:
             speak("yess boss! it's  me  your  assistant.How  can  i help  you?")
-        else:
+        elif "sleep" in request:
             speak("i  am  still  sleeping")
+        elif "what's the time" in request:
+            now_time = datetime.datetime.now().strftime("%H %M")
+            speak("Current time is " + str(now_time))
+        elif "what's the date" in request:
+            today = date.today()
+            speak("Today's date is " + str(today))
+        
 
 main_proccess()
