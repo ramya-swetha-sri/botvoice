4697059f98fd7150cbf19e6f2bfcbe2e8c51a9b4
ramya-swetha-sri <ramyaswethasri@gmail.com>
2026-07-19 22:13:08 +0530

    Add voice bot and practice script

---
+++ prac/opp1.py
@@ -0,0 +1,42 @@
+class movie:
+    pass
+m1=movie()
+m2=movie()
+m3=movie()
+print(id(m1))
+print(type(m1))
+print(id(m2))
+print(type(m2))
+print(id(m3))
+print(type(m3))
+
+
+class bankaccount:
+    pass
+ac1=bankaccount()
+ac2=bankaccount()
+print(id(ac1))
+print(type(ac1))
+print(id(ac2))
+print(type(ac2))
+
+
+class employee:
+    pass
+e1=employee()
+e2=employee()
+print(id(e1))
+print(type(e1))
+print(id(e2))
+print(type(e2))
+
+
+
+class hospital:
+    pass
+h1=hospital()
+h2=hospital()
+print(id(h1))
+print(type(h1))
+print(id(h2))
+print(type(h2))
